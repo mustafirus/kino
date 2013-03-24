5182f4e6b55d0662a42cb6e430d4a6d4ade4f269
golubev <golubev@22ebe713-2310-46eb-8452-0c763fc41ea8>
2013-03-24 11:56:20 +0000

    test http server

--- testweb++/src/testweb++.cpp
+++ testweb++/src/testweb++.cpp
@@ -7,9 +7,53 @@
 //============================================================================
 
 #include <iostream>
+#include <map>
+
+#include "web++.hpp"
+
 using namespace std;
+using namespace WPP;
+
+void web(Request* req, Response* res) {
+    cout << req->method << " " << req->path << endl;
+
+    cout << "Headers:" << endl;
+
+    map<string, string>::iterator iter;
+    for (iter = req->headers.begin(); iter != req->headers.end(); ++iter) {
+        cout << iter->first << " = " << iter->second << endl;
+    }
+
+    cout << "Query:" << endl;
+
+    for (iter = req->query.begin(); iter != req->query.end(); ++iter) {
+        cout << iter->first << " = " << iter->second << endl;
+    }
+
+    cout << "Cookies: " << req->cookies.size() << endl;
+
+    for (iter = req->cookies.begin(); iter != req->cookies.end(); ++iter) {
+        cout << iter->first << " = " << iter->second << endl;
+    }
+
+    res->body << "HELLO";
+}
+
+
+
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+    try {
+        cout << "Listening on port 5000" << endl;
+
+        WPP::Server server;
+        server.get("/", &web);
+        server.get("/zzz", &web);
+        server.all("/dir", "./");
+        server.start(5000);
+    } catch(WPP::Exception e) {
+        cerr << "WebServer: " << e.what() << endl;
+    }
+
+    return EXIT_SUCCESS;
 }
